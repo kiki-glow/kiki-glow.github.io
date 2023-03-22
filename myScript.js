a8493a70cd7f4c4b9724a169bb13df82cb7fcedb
kiki_glow <119857712+kiki-glow@users.noreply.github.com>
2023-03-23 00:17:32 +0300

    First Commit

---
+++ myScript.js
@@ -0,0 +1,35 @@
+
+function myFunction(){
+    document.getElementById("more").innerHTML = "Glow Techs is a Graphics Designing corporation that offers quite a number of services. It was launched in January 2023."
+    document.getElementById("info").innerHTML = "We offer many services including but not limited to: Web/UX Designing, Logo Designing, UI Designing, Branding and Photography."
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
